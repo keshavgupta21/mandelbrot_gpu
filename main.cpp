18f40c8933ee68a9628b15ffe6a0b9d8eb57fb74
Keshav Gupta <info@keshlabs.in>
2017-12-15 22:08:39 -0500

    lolol

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include <time.h>
 using namespace std;
 
+struct params{
+  int width = 3840, height = 2160;
+  double r_min = -2.86, r_max = 1.86, i_min = -1.325, i_max = 1.325;
+  int max = 100;
+};
+
 void bmp_write(char * img_data, int width, int height, char * filename){
   int size = height*width*3 + 54;
   ofstream bmp(filename, ios::binary);
@@ -20,7 +26,7 @@ void bmp_write(char * img_data, int width, int height, char * filename){
   bmp.close();
 }
 
-double iter(double r_c, double i_c, int max){
+__device__ double iter(double r_c, double i_c, int max){
   double r_t = 0.0, i_t = 0.0;
   double r_t_2, i_t_2;
   for (int i = 0; i < max; i++){
@@ -33,36 +39,37 @@ double iter(double r_c, double i_c, int max){
   return 0;
 }
 
-int main(int argc, char ** argv){
-  /*plotting parameters*/
-  char filename[] = "test.bmp";
-  int width = 3840, height = 2160;
-  double r_min = -2.86, r_max = 1.86, i_min = -1.325, i_max = 1.325;
-  int max = 100;
+__global__ void vecAdd(char * d_img_data, double r_min, double r_max, double i_min, double i_max, int width, int height, int max){
+    int x = blockIdx.x*blockDim.x + threadID.x;
+    int y = blockIdx.y*blockDim.y + threadID.y;
+    double r = ((double)x/(double)width)*(r_max-r_min)+r_min;
+    double i = ((double)y/(double)height)*(i_max-i_min)+i_min;
+    double t = iter(r, i, max);
+    d_img_data[y*3*width + x*3 + 0] = (char)(0*t);
+    d_img_data[y*3*width + x*3 + 1] = (char)(255*t);
+    d_img_data[y*3*width + x*3 + 2] = (char)(0*t);
+}
 
+void plot_frame(params plot, char * filename){
   /*temp vars*/
-  double r, i, t;
-  int c;
 
   /*image data generation*/
-  char * img_data = new char[width*height*3];
+  char * h_img_data = new char[plot.width*plot.height*3];
+
+  /*writing to file*/
+  bmp_write(h_img_data, plot.width, plot.height, filename);
+  delete h_img_data;
+}
+
+int main(){
+  char * filename = new char[20];
+  params plot;
   clock_t t1 = clock();
-  for (int y = 0; y < height; y++){
-    for (int x = 0; x < width; x++){
-      r = ((double)x/(double)width)*(r_max-r_min)+r_min;
-      i = ((double)y/(double)height)*(i_max-i_min)+i_min;
-      t = iter(r, i, max);
-      img_data[y*3*width + x*3 + 0] = (char)(0*t);
-      img_data[y*3*width + x*3 + 1] = (char)(255*t);
-      img_data[y*3*width + x*3 + 2] = (char)(0*t);
-    }
+  for (int i = 0; i < 1; i++){
+    sprintf(filename, "anim/%06d.bmp", i);
+    plot_frame(plot, filename);
   }
   clock_t t2 = clock();
   cout << "Took " << (double)(t2-t1)/CLOCKS_PER_SEC << " seconds.\n";
-
-  /*writing to file*/
-  bmp_write(img_data, width, height, filename);
-
-  delete img_data;
   return 0;
 }
