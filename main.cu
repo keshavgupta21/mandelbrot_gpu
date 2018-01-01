ea7685a79c97f9d80895e5525219b1109c9ef9d6
Keshav Gupta <keshav@Keshavs-MacBook-Air.local>
2018-01-01 16:15:25 +0530

    lol

--- main.cu
+++ main.cu
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <time.h>
 using namespace std;
 
 class params{
@@ -123,8 +124,25 @@ void plot_frame_cpu(params plot, char * filename){
 }
 
 int main(int argc, char ** argv){
-  char filename[] = "test.bmp";
+  int num_frames = 1000;
+  clock_t t1, t2;
+  char * filename = (char *)malloc(200*sizeof(char));
   params plot;
-  plot_frame_gpu(plot, filename);
+
+  /*TEST GPU*/
+  ofstream log_gpu("/home/keshav/Dropbox (MIT)/anim_gpu/log_gpu.txt");
+  for (int i = 0; i < num_frames; i++){
+    sprintf(filename, "/home/keshav/Dropbox (MIT)/anim_gpu/i%03d.bmp", i);
+    plot.set_frame_number(i, num_frames);
+    t1 = clock();
+    plot_frame_gpu(plot, filename);
+    t2 = clock();
+    log_gpu << 1000*(double)(t2-t1)/(CLOCKS_PER_SEC) << "\n";
+    if (!(i%(num_frames/100)) && (argc-1)) printf("Done %2.2f%% on GPU.\n",
+      100*(double)i/(double)num_frames);
+  }
+  log_gpu.close();
+
+  delete[] filename;
   return 0;
 }
