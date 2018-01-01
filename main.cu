a51af38cea2416b545f58befd7c65b7998794d3f
Keshav Gupta <keshav@Keshavs-MacBook-Air.local>
2018-01-01 12:04:01 +0530

    lol

--- main.cu
+++ main.cu
@@ -1,23 +1,44 @@
 #include <fstream>
+#include <time.h>
 using namespace std;
 
-class plot_params{
+class params{
+private:
+  double r_min_ini, r_min_trg;
+  double r_max_ini, r_max_trg;
+  double i_min_ini, i_min_trg;
+  double i_max_ini, i_max_trg;
+
 public:
-  int width, height, max;
+  int width, height;
   double r_min, r_max, i_min, i_max;
-  double epsilon;
-  plot_params();
+  int max;
+
+  params(){
+    r_min_ini = -2.86, r_min_trg = -0.1777;
+    r_max_ini = 1.86, r_max_trg = -0.1194;
+    i_min_ini = -1.33, i_min_trg = 1.0138;
+    i_max_ini = 1.33, i_max_trg = 1.0472;
+
+    width = 1920, height = 1080;
+    r_min = r_min_ini, r_max = r_max_ini, i_min = i_min_ini, i_max = i_max_ini;
+    max = 50;
+  }
+
+  void set_frame_number(int, int);
 };
 
-plot_params::plot_params(){
-  width = 1920;
-  height = 1080;
-  max = 50;
-  r_min = -3.2;
-  r_max = 3.2;
-  i_min = -1.8;
-  i_max = 1.8;
-  epsilon = 1e-6;
+void params::set_frame_number(int n, int max){
+  if (n < max){
+    double t = 1 - pow(0.001, (double)n/(double)max);
+    r_min = r_min_ini +  t*(r_min_trg - r_min_ini);
+    r_max = r_max_ini +  t*(r_max_trg - r_max_ini);
+    i_min = i_min_ini +  t*(i_min_trg - i_min_ini);
+    i_max = i_max_ini +  t*(i_max_trg - i_max_ini);
+  }
+  else{
+    r_min = r_min_trg, r_max = r_max_trg, i_min = i_min_trg, i_max = i_max_trg;
+  }
 }
 
 void bmp_write(char * img_data, int width, int height, char * filename){
@@ -37,8 +58,19 @@ void bmp_write(char * img_data, int width, int height, char * filename){
   bmp.close();
 }
 
-__device__ double iter(double r_c, double i_c, int max){
-
+__host__ __device__ double iter(double r_c, double i_c, int max){
+  double r_t = 0.0, i_t = 0.0;
+  double r_t_2, i_t_2, mag;
+  for (int i = 0; i < max; i++){
+    r_t_2 = r_t * r_t - i_t * i_t;
+    i_t_2 = 2 * r_t * i_t;
+    r_t = r_t_2 + r_c;
+    i_t = i_t_2 + i_c;
+    mag = r_t*r_t + i_t*i_t;
+    if (mag > 4){
+      return (double)(i + 1 - log(log(mag))/log(2.0))/(double)max;
+    }
+  }
   return 0;
 }
 
@@ -62,8 +94,8 @@ void plot_frame_gpu(params plot, char * filename){
   char * d_img_data;
   cudaMalloc((void **) &d_img_data, plot.width*plot.height*3*sizeof(char));
   dim3 threads(32, 32, 1);
-  dim3 grid(ceil((double)plot.width/(double)threads.x),
-    ceil((double)plot.height/(double)threads.y), 1);
+  dim3 grid(ceil((double)plot.width/32.0),
+    ceil((double)plot.height/32.0), 1);
   populate<<<grid, threads>>>(d_img_data, plot.r_min, plot.r_max, plot.i_min,
     plot.i_max, plot.width, plot.height, plot.max);
   cudaMemcpy(h_img_data, d_img_data, plot.width*plot.height*3*sizeof(char),
@@ -75,9 +107,28 @@ void plot_frame_gpu(params plot, char * filename){
   delete[] h_img_data;
 }
 
+void plot_frame_cpu(params plot, char * filename){
+  char * img_data = new char[plot.width*plot.height*3*sizeof(char)];
+  for (int y = 0; y < plot.height; y++){
+    for (int x = 0; x < plot.width; x++){
+      double r = ((double)x/(double)plot.width)*(plot.r_max-plot.r_min)+plot.r_min;
+      double i = ((double)y/(double)plot.height)*(plot.i_max-plot.i_min)+plot.i_min;
+      double t = iter(r, i, plot.max);
+      img_data[y*3*plot.width + x*3 + 0] = (char)(255*sqrt(t));
+      img_data[y*3*plot.width + x*3 + 1] = (char)(255*t);
+      img_data[y*3*plot.width + x*3 + 2] = (char)(255*t*t);
+    }
+  }
+  bmp_write(img_data, plot.width, plot.height, filename);
+  delete[] img_data;
+}
+
 int main(int argc, char ** argv){
-  char filename[20] = "test.bmp";
+  int num_frames = 10;
+  clock_t t1, t2;
+  char filename[] = "test.bmp";
   params plot;
-  plot_frame_gpu(plot)
+  plot_frame_gpu(plot, filename);
+  delete[] filename;
   return 0;
 }
